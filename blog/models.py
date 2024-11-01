57b20214944799bb65a42b51fa31d0e80a9a98b8
Barsam Hassanzadeh-aski <141170967+Bwar3am@users.noreply.github.com>
2024-11-01 10:38:21 +0330

    completed  some tsaks
    completed  some tsaks for databse schema

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import User
+
+
 
 # Create your models here.
+class blog(models.Model):
+    author = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
+    view_count = models.IntegerField(default=0)
+    title = models.CharField(max_length=100 , null=True)
+    time_created = models.TimeField(auto_now=True , null=False)
+    description = models.CharField(max_length=250 ,null=True)
+    body = models.TextField(max_length=10000 , null=False)
+    image = models.ImageField(upload_to='pics/' , default="pics/placeholder.png" , null=True)
+    date = models.DateField(null=False , default=datetime.now)
